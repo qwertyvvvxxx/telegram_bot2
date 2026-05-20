c015bb56420d21959c30d9ead13a3d62f21d8504
qwertyvvvxxx <ivannavidiatchuk@gmail.com>
2026-05-20 21:38:02 +0300

    add: comand get_routers

--- main.py
+++ main.py
@@ -6,18 +6,6 @@ from aiogram.client.default import DefaultBotProperties
 from config import TELEGRAM_TOKEN
 from handlers import get_routers
 
-
-
-
-
-
-from handlers.commands import router as commands_router
-from handlers.menu import router as menu_router
-
-
-
-
-
 bot = Bot(TELEGRAM_TOKEN, default=DefaultBotProperties(parse_mode="HTML"))
 dp = Dispatcher()
 
@@ -31,9 +19,7 @@ async def set_commands(bot: Bot):
 
 
 async def main():
-    dp.include_router(commands_router)
-
-    dp.include_router(menu_router)
+    dp.include_routers(*get_routers())
 
     await set_commands(bot)
     await bot.delete_webhook(drop_pending_updates=True)
